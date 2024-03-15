8837b612655b039769c371ef878bf4377b955064
whoischrislam <whoischrislam@gmail.com>
2024-03-15 06:53:46 -1000

    update

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-import express from 'express';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const directoryPath = dirname(fileURLToPath(import.meta.url));
+import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-
-function parseDay(number) {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    if (number === 0 || number === 6) {
-        return "weekend";
-    } else {
-        return "weekday";
-    }
-}
-
-function activityToDo(day) {
-    if (day === "weekday") {
-        return "work hard";
-    } else {
-        return "have fun";
-    }
-}
-
-app.get('/', (req, res) => {
-    const today = new Date().getDay();
-    console.log(directoryPath);
-    res.render("index.ejs", 
-    { 
-        day: parseDay(today),
-        activity: activityToDo(parseDay(today))
-    });
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
 });
 
-app.post('/check', (req, res) => {});
-
 app.listen(port, () => {
-  console.log(`Listening on port http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- solution.js
+++ solution.js
@@ -1,38 +1,18 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function passwordCheck(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
-    userIsAuthorised = true;
-  }
-  next();
-}
-app.use(passwordCheck);
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
-  }
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<em>This is some em text</em>",
+  };
+  res.render("solution.ejs", data);
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
