f3a99947e627e0c8133663442a62005da40b0470
whoischrislam <whoischrislam@gmail.com>
2024-03-15 08:32:42 -1000

    logic stuff

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
+
+
